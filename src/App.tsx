709a37f67f93b797a3833f16714e389c9b5f54a5
Cesar Augusto <47796693+CesarAugustusGroB@users.noreply.github.com>
2025-06-09 12:53:48 +0200

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ExpenseFilters from './components/ExpenseFilters/ExpenseFilters';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Expense Tracker</h1>
+      <ExpenseFilters />
+      {/* aquí podrías añadir otros componentes, ej ExpenseList */}
     </div>
   );
 };
